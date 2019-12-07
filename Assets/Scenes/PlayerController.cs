40d2020d6db08245d19e52a8478da2d2b39977e5
Skylar Masson <smasson1998@gmail.com>
2019-12-06 21:03:59 -0600

    Players move between points

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public Transform[] points;
+    int currentPoint = 0;
+
     public int playerNum = 0;
-    public string[] left = new string[] { "Horizontal", "Horizontal2" };
+    public string[] Movement = new string[] { "Horizontal", "Horizontal2" };
     //public string[] verticalAxis = new string[] { "Vertical", "Vertical2" };
     public KeyCode[] hit = new KeyCode[] { KeyCode.Joystick1Button0, KeyCode.Joystick2Button0 };
     public static Dictionary<int, PlayerController> players = new Dictionary<int, PlayerController>();
 
     void Awake() {
         players[playerNum] = this;
+        //currentPoint = points[0];
         Debug.Log("Player " + (playerNum+1).ToString());
     }
 
     void Update() {
-        if (Input.GetButtonDown(left[playerNum])) {
-            gameObject.transform.position = new Vector3(-11.8f,3.6f,-7.5f);
+        if (Input.GetButtonDown(Movement[playerNum])) {
+            currentPoint++;
+            currentPoint %= points.Length;
+            transform.position = points[currentPoint].transform.position;
         }
     }
 }
