623408a9cbd208228bfff7538c42ff6f6e2795e5
DMII Student <dmiistudent@institute.local>
2019-12-06 21:14:59 -0600

    added more ball functionality

--- Assets/Scripts/BallAction.cs
+++ Assets/Scripts/BallAction.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class BallAction: MonoBehaviour
 {
     public float speedMulti;
-    public bool locked;
+    bool locked;
     public GameObject target;
     int x = 0;
     Vector3 temp = new Vector3(0, 0, 0);
+
+    public bool Locked
+    {
+        get { return locked; }
+        set
+        {
+            locked = value;
+            GameObject explosion = Instantiate(Resources.Load("exFX") as GameObject , transform.position, Quaternion.identity);
+
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,5 +48,12 @@ public class BallAction: MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, temp, step);
         }
 
+    }
+
+    public void Reset()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        target = null;
+        speedMulti = 5.0f;
     }
 }
