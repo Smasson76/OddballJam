d3e242767da95f57458549de4572fd45c39fe5eb
Treston Larkins <treston2000@gmail.com>
2019-12-07 20:48:21 -0600

    Music, AudioManager, and Canvas Update

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioClip mainMusic;
+    private AudioSource mainMusicSource;
+
+    public AudioClip[] clips;
+    public int maxSources = 5;
+
+    private static AudioSource[] sources;
+    private static int index = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        sources = new AudioSource[maxSources];
+
+        for (int i = 0; i < maxSources; i++)
+        {
+            GameObject g = new GameObject("SFX" + i);
+            AudioSource s = g.AddComponent<AudioSource>();
+            sources[i] = s;
+        }
+
+        GameObject a = new GameObject();
+        mainMusicSource = a.AddComponent<AudioSource>();
+    }
+
+    public static void PlayMainMusic()
+    {
+        instance.mainMusicSource.clip = instance.mainMusic;
+        instance.mainMusicSource.Play();
+    }
+
+    public static void Play(string clipName)
+    {
+        AudioClip clip = null;
+        for (int i = 0; i < instance.clips.Length; i++)
+        {
+            if (instance.clips[i].name == clipName)
+            {
+                clip = instance.clips[i];
+                break;
+            }
+        }
+
+        if (clip != null)
+        {
+            sources[index].clip = clip;
+            sources[index].Play();
+            index = (index + 1) % instance.maxSources;
+        }
+    }
+}
