86140b974476a077b3ee522f2dd24b006019172c
Ton-10 <taveon30@yahoo.com>
2019-12-31 20:53:59 -0600

    repositioned a bunch of things in the MainScene, also started working on cloud generator

--- Assets/Scripts/CloudScript.cs
+++ Assets/Scripts/CloudScript.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CloudScript : MonoBehaviour
 {
     public GameObject CloudModel;
+    public float cloudSpeed = 1.0f;
 
-    int maxCloudCount = 5;
-    float cloudSpeed = 1.0f;
+    static int maxCloudCount = 5;
+    GameObject[] Clouds = new GameObject[maxCloudCount];
     // Start is called before the first frame update
+
     void Start()
     {
+        for (int i = 0; i < maxCloudCount; i++)
+        {
+            Clouds[i] = Instantiate(CloudModel, this.transform);
+            Clouds[i].transform.position = new Vector3(200, -90, 90-(18*(maxCloudCount-i)*2));
+        }
         
     }
 
